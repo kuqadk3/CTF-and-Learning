136726c26cb6e80b72f7e188f7c6a39970920b91
kuqadk3 <kuqadk3@gmail.com>
2018-08-05 17:50:06 +0700

    Update solver.c

--- CSAW 365/Crypto/Katy/solver.c
+++ CSAW 365/Crypto/Katy/solver.c
@@ -2,10 +2,15 @@
 
 int main()
 {
-    int seed = 1208101748 & 0xFFFFFFFFFFFF;
-    int last_seed = (seed - 11)/25214903917;
-    
-    printf("%i", last_seed);
+    int seed = 1364650861;
+    int i;
+    for(i = 0; i < 10000000000; i++){
+        int v0 = (25214903917 * i + 11) & 0xFFFFFFFFFFFF;
+        if(v0 == seed){
+            printf("%i", i);
+            break;
+        }
+    }
 
     return 0;
 }
