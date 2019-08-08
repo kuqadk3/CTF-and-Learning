3374c40e7f0cf7c660733687bfe017db8a889bac
kuqadk3 <kuqadk3@gmail.com>
2019-08-09 01:42:24 +0700

    Create template.cpp

---
+++ acm/template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define debug(x) cout << x << endl;
+
+int main(){
+  return 0;
+}
