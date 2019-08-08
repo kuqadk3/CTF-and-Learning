b9b3e16a149ac72244a1f7af410a110c8f76784d
kuqadk3 <kuqadk3@gmail.com>
2019-08-09 04:17:49 +0700

    Update template.cpp

--- acm/template.cpp
+++ acm/template.cpp
@@ -6,7 +6,8 @@ using namespace std;
 #define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define all(x) x.begin(),x.end()
 #define debug(x) cout << x << endl;
-#define FOR(x) for(int i =0; i <= x.size(); i++){ cout << x.at(i) << endl; }
+#define FOR(x) for(int i =0; i < (x) ; i++)
+#define FOR(a, x) for(int i = a; i <= (x); i++)
 
 int main(){
   IOS
