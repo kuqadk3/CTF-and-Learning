07caa43829cbe1d928094d92eb3bbe116324083b
kuqadk3 <kuqadk3@gmail.com>
2019-08-09 04:30:37 +0700

    Update template.cpp

--- acm/template.cpp
+++ acm/template.cpp
@@ -7,7 +7,6 @@ using namespace std;
 #define all(x) x.begin(),x.end()
 #define debug(x) cout << x << endl;
 #define FOR(x) for(int i =0; i < (x) ; i++)
-#define FOR(a, x) for(int i = a; i <= (x); i++)
 
 int main(){
   IOS
