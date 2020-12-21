2f4cac983da3c1280fb45abda32005fb82fa98b2
kuqadk3 <kuqadk3@gmail.com>
2020-12-22 02:46:16 +0700

    Update template.cpp

--- acm/template.cpp
+++ acm/template.cpp
@@ -6,10 +6,11 @@ using namespace std;
 #define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define all(x) x.begin(),x.end()
 #define debug(x) cout << x << endl;
+#define debug_vector(x) for(int i = 0; i < x.size(); i++) cout << x[i] << "\n";
 #define FOR(x) for(int i =0; i < (x) ; i++)
 
 int main(){
-  IOS
+  IOS;
   //edit
   return 0;
 }
